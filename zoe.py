024438d96f48eda8452e81a0f87ad8199ff9650b
jurchiks33 <jurisslisans@gmail.com>
2024-05-28 13:35:53 +0200

    finishing window centering and starting on a nav bar

--- zoe.py
+++ zoe.py
@@ -17,12 +17,29 @@ def create_app():
     # Background color
     root.configure(bg='#1a1a1a')
 
-    # Set the window size and position it in the center, screen size 70%
+    # Set the window screen size 70%
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     width = int(screen_width * 0.7)
     height = int(screen_height * 0.7)
+
+    # Center window on the screen
+    x_offset = int((screen_width - width) / 2)
+    y_offset = int((screen_height - height) / 2)
+
+    # Set the initial size and position of the window
+    root.geometry(f'{width} x {height} + {x_offset} + {y_offset}')
+
+    # Create a navigation bar.
+    nav_bar = ttk.Frame(root, padding="10")
+    nav_bar.pack(side="top", fill="x")
+
+    # Create content frame to switch between pages
+    content_frame = tk.Frame(root, bg='#1a1a1a')
+    content_frame.pack(fill='both', expand=True)
+
     
+
 
 
     return root
