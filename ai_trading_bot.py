1f5807a5c2bb73f5da87c0db3f5a7c959cddf8dd
jurchiks33 <jurisslisans@gmail.com>
2024-05-29 13:00:56 +0200

    AI trading boot page

--- ai_trading_bot.py
+++ ai_trading_bot.py
@@ -1,3 +1,19 @@
 #ai_trading_bot.py
 
+import tkinter as tk
+from tkinter import ttk
 
+def create_page(content_frame):
+    # Create background frame
+    frame = tk.Frame(content_frame, bg='red')
+    frame.pack(fill='both', expand=True)
+
+    #Use container frame inside to manage padding
+    container = tk.Frame(frame, bg='red', padx=12, pady=12)
+    container.pack(expand=True, fill='both')
+
+    # Place a label inside container
+    label = ttk.Label(container, text="AI-Powered Trading Bot", background='red')
+    label.pack()
+
+    return frame
