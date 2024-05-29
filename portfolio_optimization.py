b930acec5335d1d89fbb079ec49a99f0f4706aad
jurchiks33 <jurisslisans@gmail.com>
2024-05-29 13:13:31 +0200

    setting up portfolio optimization page

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -1,2 +1,19 @@
 #portfolio_optimization.py
 
+import tkinter as tk
+from tkinter import ttk
+
+def create_page(content_frame):
+    # Crete background frame
+    frame = tk.Frame(content_frame, bg='green')
+    frame.pack(fill='both', expand=True)
+
+    # Use container frame inside to manage padding
+    container = tk.Frame(frame, bg='green', padx=12, pady=12)
+    container.pack(expand=True, fill='both')
+
+    # Place label inside container
+    label = ttk.Label(container, text="Portfolio Optimization Tool", background='green')
+    label.pack()
+
+    return frame
