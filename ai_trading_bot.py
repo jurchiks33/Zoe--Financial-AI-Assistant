acf7945179bb46dc7ab1dbb9ec583cac65c08e71
jurchiks33 <jurisslisans@gmail.com>
2024-05-31 15:25:16 +0200

    working on a buttons

--- ai_trading_bot.py
+++ ai_trading_bot.py
@@ -63,6 +63,15 @@ def create_page(content_frame):
     symbol_entry = ttk.Entry(container)
     symbol_entry.pack(pady=5)
 
+    def on_submit():
+        symbol = symbol_entry.get().upper()
+        clear_frame(container)
+        plot_stock_data(symbol, container)
+        simulate_trading_strategy(symbol, container)
+    
+    submit_button = ttk.Button(container, text='Submit', command=on_submit)
+    submit_button.pack(pady=5)
+
 
 
 
