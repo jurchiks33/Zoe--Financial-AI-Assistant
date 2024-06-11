730b9d52cc294bd6f04e01a85277e5c1702f164b
jurchiks33 <jurisslisans@gmail.com>
2024-06-11 12:58:20 +0200

    function for stock data plotting

--- ai_trading_bot.py
+++ ai_trading_bot.py
@@ -6,3 +6,17 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+#Function for stock data plotting
+def plot_stock_data(symbol, container):
+    stock_data = yf.download(symbol, period = "1y")
+    fig, ax = plt.subplots()
+    ax.plot(stock_data['Close'], label='Close Price')
+    ax.set_title(f'{symbol} Closing Prices')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.legend()
+
+    canvas = FigureCanvasTkAgg(fig, master=container)
+    canvas.draw()
+    canvas.get_tk_widget().pack(expand=True, fill='both')
+
