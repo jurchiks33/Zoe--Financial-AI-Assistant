5be3859b2a133e03b5ce2551a557084dc039b232
jurchiks33 <jurisslisans@gmail.com>
2024-09-16 21:55:01 +0200

    frame 6

---
+++ ai_trading_bot/frames/frame6.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def create_frame6(parent):
+    frame6 = tk.Frame(parent, bg='#333333', bd=2, relief='solid')
+    frame6.place(relx=0.228, rely=0.05, relwidth=0.06, relheight=0.43)  # Long vertical left
+
+    # frame logic is coming below
+    label6 = tk.Label(frame6, text="Frame6", bg='#333333', fg='white')
+    label6.pack(expand=True)
+
+    return frame6
+
