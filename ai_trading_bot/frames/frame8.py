7d77ca515c721a62c3579eb0c4e700fa39dabbc5
jurchiks33 <jurisslisans@gmail.com>
2024-09-16 22:00:29 +0200

    frame 8

---
+++ ai_trading_bot/frames/frame8.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def crate_frame8(parent):
+    frame8 = tk.Frame(parent, bg='#333333', bd=2, relief='solid')
+    frame8.place(relx=0.15, rely=0.49, relwidth=0.7, relheight=0.2)
+
+    # frame logic coming below
+    label8 = tk.Label(frame8, text="Frame8", bg='#333333', fg='white')
+    label8.pack(expand=True)
+
+    return frame8
