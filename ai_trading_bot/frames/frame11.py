2ef8586d278c8eeabe9e7647cf5dd63ae4c32be1
jurchiks33 <jurisslisans@gmail.com>
2024-09-16 22:06:58 +0200

    frame 11

---
+++ ai_trading_bot/frames/frame11.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def create_frame11(parent):
+    frame11 = tk.Frame(parent, bg='#333333', bd=2, relief='solid')
+    frame11.place(relx=0.02, rely=0.7, relwidth=0.96, relheight=0.26)
+
+    # frame logic coming below
+    label11 = tk.Label(frame11, text="Frame11", bg='#333333', fg='white')
+    label11.pack(expand=True) 
+
+    return frame11
