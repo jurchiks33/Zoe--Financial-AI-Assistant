3cb8beb03f0c31e78e710e26e308b398bd22a462
jurchiks33 <jurisslisans@gmail.com>
2024-09-19 22:06:34 +0200

    debugging

--- ai_trading_bot/frames/frame11.py
+++ ai_trading_bot/frames/frame11.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 def create_frame11(parent):
     frame11 = tk.Frame(parent, bg='#333333', bd=2, relief='solid')
-    frame11.place(relx=0.02, rely=0.7, relwidth=0.45, relheight=0.26)  # Correctly using place()
+    frame11.place(relx=0.02, rely=0.7, relwidth=0.1, relheight=0.26)
     
     # frame logic coming below
     label11 = tk.Label(frame11, text="Frame 11", bg='#333333', fg='white')
